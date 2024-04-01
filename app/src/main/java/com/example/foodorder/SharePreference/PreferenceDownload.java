6b990b9ac994524a38bc360742fb05518792cb51
TongTuan <ttong6913@gmail.com>
2024-04-01 22:50:13 +0700

    check dang nhap lan dau tien

---
+++ app/src/main/java/com/example/foodorder/SharePreference/PreferenceDownload.java
@@ -0,0 +1,25 @@
+package com.example.foodorder.SharePreference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.foodorder.Constants.StateDownload;
+
+public class PreferenceDownload {
+    private static final String SHARE_PREFERENCE="SHARE_PREFERENCE";
+    private Context context;
+
+    public PreferenceDownload(Context context) {
+        this.context = context;
+    }
+    public void setValueDownload(String key, StateDownload state){
+        SharedPreferences sharedPreferences=context.getSharedPreferences(SHARE_PREFERENCE,context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putInt(key,state.ordinal());
+        editor.apply();
+    }
+    public int getValueDownload(String key){
+        SharedPreferences sharedPreferences=context.getSharedPreferences(SHARE_PREFERENCE,context.MODE_PRIVATE);
+        return sharedPreferences.getInt(key,StateDownload.FIRSTDOWLOAD.ordinal());
+    }
+}
