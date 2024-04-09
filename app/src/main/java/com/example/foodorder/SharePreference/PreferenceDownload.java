dc6cc4d2a4cc33e3cf1e2f059526a0c27795ff0f
TongTuan <ttong6913@gmail.com>
2024-04-09 23:04:45 +0700

    cc

--- app/src/main/java/com/example/foodorder/SharePreference/PreferenceDownload.java
+++ app/src/main/java/com/example/foodorder/SharePreference/PreferenceDownload.java
@@ -7,19 +7,19 @@ import com.example.foodorder.Constants.StateDownload;
 
 public class PreferenceDownload {
     private static final String SHARE_PREFERENCE="SHARE_PREFERENCE";
-    private Context context;
+    private final Context context;
 
     public PreferenceDownload(Context context) {
         this.context = context;
     }
     public void setValueDownload(String key, StateDownload state){
-        SharedPreferences sharedPreferences=context.getSharedPreferences(SHARE_PREFERENCE,context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences=context.getSharedPreferences(SHARE_PREFERENCE, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
         editor.putInt(key,state.ordinal());
         editor.apply();
     }
     public int getValueDownload(String key){
-        SharedPreferences sharedPreferences=context.getSharedPreferences(SHARE_PREFERENCE,context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences=context.getSharedPreferences(SHARE_PREFERENCE, Context.MODE_PRIVATE);
         return sharedPreferences.getInt(key,StateDownload.FIRSTDOWLOAD.ordinal());
     }
 }
